daf27edd12765af7a192584795d5544b6b95c1ad
Azwarranda <azwariranda@gmail.com>
2024-04-29 01:03:29 +0700

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,225 @@
+export const Bio = {
+  name: "Azwar Iranda",
+  roles: [
+    "Web Developer",
+    "Frontend Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "Saya adalah individu yang termotivasi, selalu bersemangat untuk menghadapi tantangan baru. Dengan semangat untuk belajar, saya berdedikasi untuk memberikan hasil berkualitas tinggi. Dengan sikap positif dan pola pikir berkembang, saya siap memberikan kontribusi yang berarti dan mencapai hal-hal hebat.",
+  github: "https://github.com/Azwarranda",
+  resume:
+    "https://drive.google.com/file/d/1eG7Oestid7xT5VBJZogHrY5I8nXPQtBm/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Sport",
+    skills: [
+      {
+        name: "Pencak Silat",
+        image: "https://png.pngtree.com/png-clipart/20201208/original/pngtree-pencak-silat-traditional-martial-art-png-image_5574616.jpg",
+      },
+      {
+        name: "Sepak Bola",
+        image:
+          "https://i.pinimg.com/564x/d9/a3/ad/d9a3ad7ba6d9756be016aeee0650ef30.jpg",
+      },
+      {
+        name: "Mobile Legend",
+        image: "https://i.pinimg.com/564x/ca/0b/78/ca0b785032a23a9c185aa5ca8aae69d7.jpg",
+      },
+      {
+        name: "Badminton",
+        image:
+          "https://i.pinimg.com/236x/1d/09/b6/1d09b6860160e069e35f256a09a15f6d.jpg",
+      },
+      {
+        name: "Main Gitar",
+        image:
+          "https://i.pinimg.com/564x/bb/73/84/bb73846c55ab70ac73ad5d0385639b69.jpg",
+      },
+    ],
+  },
+  {
+    title: "Microsoft",
+    skills: [
+      {
+        name: "Ms.Word",
+        image:
+          "https://cdn.pixabay.com/photo/2021/01/30/12/20/microsoft-word-5963679_1280.png",
+      },
+      {
+        name: "Ms.Exel",
+        image:
+          "https://cdn.pixabay.com/photo/2021/01/30/12/15/excel-5963669_1280.png",
+      },
+      {
+        name: "Power Point",
+        image:
+          "https://cdn.pixabay.com/photo/2021/01/30/12/18/powerpoint-5963677_640.png",
+      },
+      {
+        name: "Visio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Microsoft_Office_Visio_%282019%29.svg/2149px-Microsoft_Office_Visio_%282019%29.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Adobe Premiere",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Adobe_Premiere_Pro_CC_icon.svg/2101px-Adobe_Premiere_Pro_CC_icon.svg.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+      {
+        name: "Adobe Illustrator",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Adobe_Illustrator_CC_icon.svg/2101px-Adobe_Illustrator_CC_icon.svg.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://w7.pngwing.com/pngs/54/524/png-transparent-figma-app-logo-tech-companies-thumbnail.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdPWgTxp6do5gnZFezRaxQELAF2GpkVA1vkhc0rTOyJg&s",
+    role: "Security Uniqlo Grand Batam Mall",
+    company: "PT. Sierra Solution",
+    date: "juni 2020 - Januari 2021",
+    desc: "Bekerja sebagai tenaga keamanan di Uniqlo grand batam mall selama setahun dan terjaga dengan baik.",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoG2dea0zASzX2xPFnNIjrEQNOh3f8hVCF8MyfTUnhqg&s",
+    role: "Marketing Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
+    company: "STT Indonesia Tanjungpinang",
+    date: "June 2023 - September 2023",
+    desc: "Bekerja sebagai Brand Ambasador dan sebagai marketing untuk mempromosikan kampus ke sekolah yang ada di tanjungpinang.",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPP2qaZwjdT5EydbaQfAnr0DMV6mGJy55W4V93qPyIiw&s",
+    role: "Waitress & Barista",
+    company: "Kafella Restaurant",
+    date: "July 2022 - september 2022",
+    desc: "Bekerja sebagai waiters dan juga sempat menjadi Barista di restoran Kafella.",
+  },
+  {
+    id: 2,
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVyVG1tOXHUICH4UUM61xcz9FH5-6SVApxOwlmwn4pNxKamEWaSmKI2jDfXOTqG83zUp00a-FE4cKMt5z0WvArOLW0exqF-IUXLE2zoqb0BKsuteiw3QxzVmRgCnHXvDo0T2kySc3uF5c/w1200-h630-p-k-no-nu/20180428_135549.jpg",
+    role: "Stanby ",
+    company: "PT. Sumber Rezeki",
+    date: "May 2023 - Present",
+    desc: "Bekerja sebagai stanby di supermarket dan sebagai pengecek barang jika stok ada yang kosong.",
+  },
+  {
+    id: 3,
+    img: "https://storage.googleapis.com/kampusmerdeka_kemdikbud_go_id/mitra/mitra_f6fccdce-1cdb-4977-adf2-b4b5c3c52cc8.png",
+    role: "Web Development",
+    company: "Infinite Learning",
+    date: "Februari 2024 - Present",
+    desc: "Mengikuti kegiatan MSIB yang di selengarakan di Infinite learning yang dimana Infinite Learning menciptakan para development muda yang siap untuk Indonesia Maju.",
+  },
+  {
+    id: 4,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhoi_8Z6QPcp9xLeyaoDnfobUwZfhmrZyTktFVquKEwA&s",
+    role: "Security Perbankan",
+    company: "BNI Tanjungpinang",
+    date: "Maret 2021 - Agustus 2021",
+    desc: "Bekerja Sebagai Tenaga Keamanan Di Bank BNI yang kurang lebih 5 Bulan. ",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbLGGe1SD82wgr-hChPI5_qogVISyQNoJqQIsH9booTQ&s",
+    school: "SDN 005 Tanjungpinang Kota",
+    date: "januari 2008 - Mei 2014",
+    grade: "90.75%",
+    desc: "Saya menduduki bangku SD 005 Tanjungpinang di tempat tinggal saya yang berada di kamopung bugis kota tanjungpinang saya merupakan siswa yang sangat calm dan sangat rain mengerjakan PR ",
+    degree: "Sekolah Dasar",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAOBYuyDmxbIA9uGXUOCWzZ7WLJkuUwkWgLWqEEJVGTw&s",
+    school: "SMPN 11 Tanjungpinang Kota",
+    date: "Agustus 2014 - July 2017",
+    grade: "80.2%",
+    desc: "Saya menyelesaikan bangku sekolah saya di SMP negri 11 tanjungpinang kurang lebih 3 tahun dan banyak pencapaian yang saya raih ditingkat SMP mulai dari academik dan non academik",
+    degree: "Sekolah Menengah Pertama",
+  },
+  {
+    id: 2,
+    img: "https://i0.wp.com/www.sman6-tpi.sch.id/wp-content/uploads/2017/01/logo200.png?resize=200%2C200",
+    school: "SMAN 6 Tanjungpinang Kota",
+    date: "Apr 2017 - Apr 2020",
+    grade: "77.3%",
+    desc: "Saya menyelesaikan bangku sekolah saya di SMA N 6 tanjungpinang kurang lebih 3 tahun dan banyak pencapaian yang saya raih ditingkat SLTA sederajat mulai dari academik dan non academik.",
+    degree: "Sekolah Menengah Atas",
+  },
+];
+
